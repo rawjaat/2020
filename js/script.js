362fd56aa3f05d7c185222236ad6eee6191cbc81
rawjaat <dvanakataajwar@gmail.com>
2021-03-15 21:51:12 -0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -242,51 +242,6 @@ function hidePreloader() {
 
 	
 	
-/*-------------------------------------------
-Functions Show / Hide Preloader
----------------------------------------------*/
-
-var slideIndex1 = 0;
-showSlides1();
-
-function showSlides1() {
-  var j;
-  var slides1 = document.getElementsByClassName("mySlides1");
-  var dots1 = document.getElementsByClassName("dot1");
-  for (j = 0; j < slides1.length; j++) {
-    slides1[j].style.display = "none";  
-  }
-  slideIndex1++;
-  if (slideIndex1 > slides1.length) {slideIndex1 = 1}    
-  for (j = 0; j < dots1.length; j++) {
-    dots1[j].className = dots1[j].className.replace(" active", "");
-  }
-  slides1[slideIndex1-1].style.display = "block";  
-  dots1[slideIndex1-1].className += " active";
-  setTimeout(showSlides1, 6000); // Change image every X000 seconds
-}
-	
-	
-	
-var slideIndex = 0;
-showSlides();
-
-function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 4500); // Change image every X000 seconds
-}
 
 
 })//End
